ab0424e17d8b9adb4e459ea8ac7b0677f1a4968b
IllicitFrog <108371172+IllicitFrog@users.noreply.github.com>
2025-05-28 12:39:56 -0600

    Update project_config.lua
    Add simple formatting to config file for readability

--- lua/tasks/project_config.lua
+++ lua/tasks/project_config.lua
@@ -1,6 +1,50 @@
 local config = require('tasks.config')
 local Path = require('plenary.path')
 
+--- Formats a JSON string with indentation.
+---@param json_str string
+---@param indent integer
+---@return string
+local function format_json(json_str, indent)
+  indent = indent or 2
+  local function newline(level) return '\n' .. string.rep(' ', indent * level) end
+
+  local i, len = 1, #json_str
+  local level = 0
+  local out, in_string, prev_char = {}, false, ''
+  while i <= len do
+    local char = json_str:sub(i, i)
+    if char == '"' and prev_char ~= '\\' then
+      in_string = not in_string
+      table.insert(out, char)
+    elseif not in_string then
+      if char == '{' or char == '[' then
+        table.insert(out, char)
+        level = level + 1
+        table.insert(out, newline(level))
+      elseif char == '}' or char == ']' then
+        level = level - 1
+        table.insert(out, newline(level))
+        table.insert(out, char)
+      elseif char == ',' then
+        table.insert(out, char)
+        table.insert(out, newline(level))
+      elseif char == ':' then
+        table.insert(out, ': ')
+      elseif char:match('%s') then
+        -- ignore whitespace
+      else
+        table.insert(out, char)
+      end
+    else
+      table.insert(out, char)
+    end
+    prev_char = char
+    i = i + 1
+  end
+  return table.concat(out)
+end
+
 --- Contains all fields from configuration.
 ---@class ProjectConfig
 local ProjectConfig = {}
@@ -25,7 +69,8 @@ function ProjectConfig:write()
   local params_file = Path:new(config.params_file)
   local tmp_dap_open_command = self.dap_open_command
   self.dap_open_command = nil
-  params_file:write(vim.json.encode(self), 'w')
+  local json = vim.json.encode(self)
+  params_file:write(format_json(json), 'w')
   self.dap_open_command = tmp_dap_open_command
 end
 
